0798c176b7004a80dc0deee1f9c63e9c0c44af57
Bruno Randolf <br1@einfach.org>
2019-01-28 12:14:41 +0000

    Add some common macros

--- blzlib_util.h
+++ blzlib_util.h
@@ -5,8 +5,25 @@
 #include <stdint.h>
 
 /* for use in printf-like functions */
+#ifndef MAC_FMT
 #define MAC_FMT "%02x:%02x:%02x:%02x:%02x:%02x"
+#endif
+
+#ifndef MAC_PAR
 #define MAC_PAR(x) x[0], x[1], x[2], x[3], x[4], x[5]
+#endif
+
+#ifndef MAX
+#define MAX(_x, _y) ((_x) > (_y) ? (_x) : (_y))
+#endif
+
+#ifndef MIN
+#define MIN(_x, _y) ((_x) < (_y) ? (_x) : (_y))
+#endif
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
+#endif
 
 bool blz_string_to_mac(const char* str, uint8_t mac[6]);
 uint8_t* blz_string_to_mac_s(const char* str);
